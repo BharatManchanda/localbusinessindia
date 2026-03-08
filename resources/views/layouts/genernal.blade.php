d28bf24bece0d5d6fc7327bfd0d9977d2099ba08
Bharat Manchanda <bharatmanchanda13@gmail.com>
2026-03-08 09:42:13 +0530

    update

--- resources/views/layouts/genernal.blade.php
+++ resources/views/layouts/genernal.blade.php
@@ -4,8 +4,11 @@
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <meta name="robots" content="noindex, nofollow">
-        <link rel="stylesheet" href="{{ asset('/assets/bootstrap-5.3.3-dist/css/bootstrap.css') }}"></link>
-        <script src="{{ asset('/assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.js') }}" ></script>
+        <link
+            rel="stylesheet"
+            href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
+            onerror="this.onerror=null;this.href='{{ asset('assets/bootstrap-5.3.3-dist/css/bootstrap.min.css') }}';"
+        >
         @yield('import.header.files')
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
         <title>@yield('title', 'My App')</title>
@@ -107,5 +110,9 @@
         </div>
     </footer>
     <x-toast />
+    <script
+        src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
+        onerror="this.onerror=null;this.src='{{ asset('assets/bootstrap-5.3.3-dist/js/bootstrap.bundle.min.js') }}';"
+    ></script>
 </body>
 </html>
